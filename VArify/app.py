ae814925c61f4fb76790101d6b4f9ada76119d25
Brian Ross <brian.ross@students.makeschool.com>
2021-02-24 03:39:32 -0500

    deployed to flask and attempt at making api call work was made

--- VArify/app.py
+++ VArify/app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request, send_file
 from pprint import PrettyPrinter
 import os
+import requests
 
 app = Flask(__name__, template_folder="templates")
 
@@ -15,11 +16,45 @@ pp = PrettyPrinter(indent=4)
 @app.route('/')
 def page():
     """Display the web page."""
-    va_search = request.args.get('name-query')
-    print(va_search)
+
+    #Getting the first and last name of a character in an anime for use in our API call. 
+    first_name = request.args.get('first-name-query')
+    last_name = request.args.get('last-name-query')
+
+    url = 'https://private-2139da-animedb.apiary-mock.com/api/characters'
+
+    # # params = {
+
+    # #     # 'app_id': '8f2c6d67',
+    # #     # 'app_key': 'e5e5bdc2fdb7f36e504cc8478d9f88fe',
+    # #      'first_name': first_name
+    # #     # 'last_name': last_name
+
+    # # }
+
+    # result_json = requests.get(url).json()
+    # # pp.pprint(result_json)
+    context = {
+        # 'description': result_json[0]['description'],
+        'first_name': first_name,
+        'last_name':last_name
+        
 
 
-    return render_template('index.html')
+    }
+
+    return render_template('index.html', **context)
+
+@app.route('/shows')
+def shows():
+    """returns a bunch of stuff"""
+    return render_template('shows.html')
+
+
+
+
+
+    
 
 
 if __name__ == '__main__':
