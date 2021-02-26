88c64d55fcf1c7e4d2f130b40a8b8666605e4e9f
Brian Ross <brian.ross@students.makeschool.com>
2021-02-26 06:06:41 -0500

    working api call. you can now search for voice actors by name

--- VArify/app.py
+++ VArify/app.py
@@ -14,56 +14,81 @@ API_KEY = os.getenv('API_KEY')
 pp = PrettyPrinter(indent=4)
 
 
+
 @app.route('/')
 def page():
     """Display the web page."""
 
     full_name = request.args.get('full-name')
+    
     character_list = []
+    
 
-
-    query = '''
-    query ($name: String) {
-        Staff(search: $name) {
-            id
-            characters{
-                nodes {
-                    id
-                    name {
-                        full
+    query = '''query ($name: String) {
+    Character(search: $name) {
+        id
+        name {
+        full
+        }
+        image {
+        large
+        }
+        media {
+        edges {
+            voiceActors {
+            image{
+                large
             }
+            name {
+                full
             }
+            language
+            }
+            node {
+            title {
+                romaji
+                english
+                native
+                userPreferred
+            }
+            coverImage{
+                large
+            }
+            type
             }
         }
         }
-        '''
+    }
+    }
+    '''
     variables = {
-    'name': full_name
-        }
+        'name': full_name
+    }
 
     url = 'https://graphql.anilist.co'
 
-    
-    result_json = requests.post(url, json={'query': query, 'variables': variables})
+    result_json = requests.post(
+        url, json={'query': query, 'variables': variables})
     json_response = json.loads(result_json.text)
-    
 
-    for i in range(len(json_response["data"]["Staff"]["characters"]["nodes"])):
-        character_list.append(json_response["data"]["Staff"]["characters"]["nodes"][i]["name"])
-    
-    
+    # pp.pprint(json_response)
+    character_name = []
+    for i in range(len(json_response["data"]["Character"]["media"]["edges"])):
+        character_list = json_response["data"]["Character"]["media"]["edges"][i]['voiceActors']
+        for i in character_list:
+            character_name.append(i["name"])
+    print(character_name)       
 
-    # while json_response["data"]["Staff"]["characters"]["nodes"][i] != None:
-    #     print(i["name"])
-    
     
     context = {
         'full_name': full_name,
-        "character_list": character_list
+        "character_list": character_name[0]["full"]
         
        
     }
     return render_template('index.html', **context)
+
+
 
 @app.route('/shows')
 def shows():
